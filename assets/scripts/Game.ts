423fd3e7d215b73557b6a1595601e92415f40cac
RedGrey <975710161@qq.com>
2023-06-16 19:20:40 +0800

    Implement all move direction and merge operation.

--- assets/scripts/Game.ts
+++ assets/scripts/Game.ts
@@ -23,11 +23,16 @@ export class Game extends Component {
     @property({type: Node})
     private background: Node;
     private blockSize: number = 0;
-    private blockPositions: Vec2[][] = [];
-    private blocks = [];
+    private blockPositions: Vec3[][] = [];
+    private blocks: Node[][] = [];
     private startPosition: Vec2;
     private endPosition: Vec2;
     private moveDirection = [0, -1]; //默认向左移动
+    private canOperate = true;
+    private acuallyMoved = false;   // 如果滑动事件后，没有一个格子能移动，则不新增格子
+    // 前一次滑动是否是相同分数的合并，避免连环合并的情况
+    // 如: 2 2 4，向左划，应该是4 4，而不应该是8
+    private prevMerged: boolean[][] = [];
 
     drawBgBlocks() {
         this.blockSize = 
@@ -37,13 +42,13 @@ export class Game extends Component {
         var y = this.gap + this.blockSize / 2;
         for (var i=0;i<gBlockVerticalNum;++i) {
             var x = this.gap + this.blockSize / 2;
-            this.blockPositions.push(new Array<Vec2>(gBlockHorizontalNum));
+            this.blockPositions.push(new Array<Vec3>(gBlockHorizontalNum));
             for(var j=0;j<gBlockHorizontalNum;++j) {
                 var block = instantiate(this.blockPrefab);
                 var blockUiTransform = block.getComponent(UITransform);
                 blockUiTransform.width = blockUiTransform.height = this.blockSize;
-                block.setPosition(x, y);
-                this.blockPositions[i][j] = new Vec2(x, y);
+                block.setPosition(x, y, 0);
+                this.blockPositions[i][j] = new Vec3(x, y, 1);
                 block.getComponent(Block).setNumber(0);
                 this.background.addChild(block);
                 x += this.gap + this.blockSize;
@@ -67,7 +72,7 @@ export class Game extends Component {
 
         this.blocks = [];
         for (var i=0;i<gBlockVerticalNum;++i) {
-            this.blocks.push(new Array(gBlockHorizontalNum));
+            this.blocks.push(new Array<Node>(gBlockHorizontalNum));
             for (var j=0;j<gBlockHorizontalNum;++j) {
                 this.blocks[i][j] = null;
             }
@@ -110,7 +115,7 @@ export class Game extends Component {
         var block = instantiate(this.blockPrefab);
         var blockUiTransform = block.getComponent(UITransform);
         blockUiTransform.width = blockUiTransform.height = this.blockSize;
-        block.setPosition(pos.x, pos.y);
+        block.setPosition(pos);
         // console.log(gRandomGenerateNumbers[randomRangeInt(0, gRandomGenerateNumbers.length)]);
         block.getComponent(Block).setNumber(gRandomGenerateNumbers[randomRangeInt(0, gRandomGenerateNumbers.length)]);
         this.background.addChild(block);
@@ -119,48 +124,127 @@ export class Game extends Component {
         return true;
     }
 
-    moveAninmation() {
-        for(var i=0;i<gBlockHorizontalNum;++i) {
-            for(var j=0;j<gBlockVerticalNum;++j) {
-                if (j > 0 && this.blocks[i][j] != null) {
-                    var t = tween(this.blocks[i][j]);
-                    t.to(0.5, {worldPosition: new Vec3(this.blockPositions[i][j-1].x, this.blockPositions[i][j-1].y, 1)}, {easing: 'backOut'});
-                    t.call(()=>{
-                        
-                    })
-                    t.start();
+    moveAninmation(block: Node, pos: Vec3, callback) {
+        this.acuallyMoved = true;
+        var t = tween(block);
+        t.to(0.01, {worldPosition: pos}, {easing: 'backOut'});
+        t.call(()=>{
+            callback && callback();
+        })
+        t.start();
+    }
+
+    inRange(x, y) {
+        return x >= 0 && x < gBlockHorizontalNum && y >=0 && y < gBlockVerticalNum;
+    }
+
+    move(x, y, callback) {
+        var tx = x + this.moveDirection[0];
+        var ty = y + this.moveDirection[1];
+
+        if (!this.inRange(tx, ty)) {
+            callback && callback();
+        } else if (this.blocks[tx][ty] == null) {
+            var pos = this.blockPositions[tx][ty];
+            this.blocks[tx][ty] = this.blocks[x][y]
+            this.blocks[x][y] = null;
+            this.moveAninmation(this.blocks[tx][ty], pos, () => {
+                this.move(tx, ty, callback);
+            });
+        } else if (this.blocks[tx][ty].getComponent(Block).getNumber() ==
+            this.blocks[x][y].getComponent(Block).getNumber()) {
+            if (this.prevMerged[tx][ty]) {
+                callback && callback();
+                return;
+            }
+            var pos = this.blockPositions[tx][ty];
+            this.moveAninmation(this.blocks[x][y], pos, () => {
+                var num = this.blocks[tx][ty].getComponent(Block).getNumber();
+                this.blocks[tx][ty].getComponent(Block).setNumber(num * 2);
+                this.background.removeChild(this.blocks[x][y]);
+                this.blocks[x][y].destroy();
+                this.blocks[x][y] = null;
+                this.prevMerged[tx][ty] = true;
+                callback && callback();
+            });
+        } else { //和前一个格子数字不一样
+            callback && callback();
+        }
+    }
+
+    doMove(startx, endx, starty, endy, stepx, stepy) {
+        var toMove = [];
+        for (var i:number=startx;i!=endx;i+=stepx) {
+            for(var j=starty;j!=endy;j+=stepy) {
+                if (this.blocks[i][j] != null) {
+                    toMove.push([i,j]);
                 }
             }
         }
+        var cur = 0;
+        var moveFinish = () => {
+            cur++;
+            if (cur == toMove.length) {
+                if (this.acuallyMoved) {
+                    this.addRandomNumberBlock();
+                }
+                this.canOperate = true;
+            } else {
+                this.move(toMove[cur][0], toMove[cur][1], moveFinish);
+            }
+        };
+        this.move(toMove[cur][0], toMove[cur][1], moveFinish);
     }
 
     moveRight() {
         console.log('move right');
         this.moveDirection = [0, 1];
+        var startx=0, endx=gBlockHorizontalNum;
+        var starty=gBlockVerticalNum-1, endy=-1;
+        this.doMove(startx, endx, starty, endy, 1, -1);
     }
 
     moveLeft() {
         console.log('move left');
         this.moveDirection = [0, -1];
-        this.moveAninmation();
+        var startx=0, endx=gBlockHorizontalNum;
+        var starty=0, endy=gBlockVerticalNum;
+        this.doMove(startx, endx, starty, endy, 1, 1);
     }
 
     moveUp() {
         console.log('move up');
         this.moveDirection = [1, 0];
+        var startx=gBlockHorizontalNum-1, endx=-1;
+        var starty=0, endy=gBlockVerticalNum;
+        this.doMove(startx, endx, starty, endy, -1, 1);
     }
 
     moveDown() {
         console.log('move down');
         this.moveDirection = [-1, 0];
+        var startx=0, endx=gBlockHorizontalNum;
+        var starty=0, endy=gBlockVerticalNum;
+        this.doMove(startx, endx, starty, endy, 1, 1);
     }
 
     touchEnd(event) {
+        if (this.canOperate == false) {
+            return;
+        }
         this.endPosition = event.getLocation();
         // console.log(this.endPosition);
         var dirVec = this.endPosition.subtract(this.startPosition);
         // console.log(dirVec);
         if (dirVec.length() > gTouchMoveThreshold) {
+            this.canOperate = false;
+            this.acuallyMoved = false;
+            for(var i=0;i<gBlockHorizontalNum;++i) {
+                this.prevMerged[i]=[];
+                for(var j=0;j<gBlockVerticalNum;++j) {
+                    this.prevMerged[i][j] = false;
+                }
+            }
             if (Math.abs(dirVec.x) > Math.abs(dirVec.y)) { // 水平滑动
                 if (dirVec.x > 0) {
                     this.moveRight();
